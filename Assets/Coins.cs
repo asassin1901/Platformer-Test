4ceb1ecbdd1d8fe4f690e54ce765b0563339ef52
asassin1901 <57683620+asassin1901@users.noreply.github.com>
2022-02-17 16:43:30 +0100

    coins
    - Added coin functionality
    - Added coin ui elements

---
+++ Assets/Coins.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coins : MonoBehaviour
+{
+    public Text coinCount;
+
+    public float coins;
+
+    private void Update()
+    {
+        coinCount.text = coins.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        coinCount.GetComponent<Coins>().coins++;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController2D controller = null;
+
+    float horizontal = 0f;
+
+    public float moveSpeed = 30f;
+
+    public bool jump = false;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontal * moveSpeed * Time.fixedDeltaTime, false, jump);
+
+        jump = false;
+    }
+}
